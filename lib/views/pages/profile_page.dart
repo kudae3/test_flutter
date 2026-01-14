7aef58db000df25f5968df68f5f5f40b2c8d62d8
Kudae Sithu <kudaesithu2@gmail.com>
2026-01-14 12:09:53 +0630

    refactor ProfilePage: replace welcome message with logout functionality

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:my_project_1/data/notifiers.dart';
+import 'package:my_project_1/views/pages/welcome_page.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('Welcome to the Profile Page!'),
-      );
+    return ListTile(
+      leading: Icon(Icons.logout),
+      title: Text('Logout'),
+      onTap: () {
+        currentIndexNotifier.value = 0;
+        Navigator.pushReplacement(context, 
+          MaterialPageRoute(builder: (context) {
+            return WelcomePage();
+          })
+        );
+      },
+    );
   }
 }
