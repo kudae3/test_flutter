e20b2a94856afdef03bc3c70101515396977337f
Kudae Sithu <kudaesithu2@gmail.com>
2026-01-15 16:14:02 +0630

    feat: update app icons and manifest files for improved branding
    - Changed Android app icon reference in AndroidManifest.xml from ic_launcher to launcher_icon.
    - Added new launcher icons for various resolutions in mipmap directories.
    - Created flutter_launcher_icons.yaml for managing app icons across platforms.
    - Updated iOS project settings to use new app icon assets.
    - Modified Contents.json for iOS app icons to reflect new asset structure.
    - Updated welcome_page.dart to enhance UI with SingleChildScrollView.
    - Updated web manifest and favicon for consistent branding.
    - Added new icons for web and Windows platforms.

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -12,57 +12,59 @@ class WelcomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Lottie.asset('assets/lotties/Fish_Loader.json'),
-              FittedBox(child: Text('Welcome to the App!', style: TextStyle(fontSize: 30.0, letterSpacing: 10.0))),
-              SizedBox(height: 23),
-              FilledButton(
-                style: ElevatedButton.styleFrom(
-                  // padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20)
-                  minimumSize: Size(double.infinity, 50)
-                ),
-                onPressed: () => {
-                  Navigator.push(context, 
-                    MaterialPageRoute(builder: (context) {
-                      return OnboardingPage();
-                    })
-                  )
-                }, 
-              child: const Text(
-                  'Get Started',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/lotties/Fish_Loader.json'),
+                FittedBox(child: Text('Welcome to the App!', style: TextStyle(fontSize: 30.0, letterSpacing: 10.0))),
+                SizedBox(height: 23),
+                FilledButton(
+                  style: ElevatedButton.styleFrom(
+                    // padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20)
+                    minimumSize: Size(double.infinity, 50)
+                  ),
+                  onPressed: () => {
+                    Navigator.push(context, 
+                      MaterialPageRoute(builder: (context) {
+                        return OnboardingPage();
+                      })
+                    )
+                  }, 
+                child: const Text(
+                    'Get Started',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
-              
-              SizedBox(height: 10),
-              
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  // padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                  minimumSize: Size(double.infinity, 50)
-                ),
-                onPressed: () => {
-                  Navigator.push(context, 
-                    MaterialPageRoute(builder: (context) {
-                      return LoginPage();
-                    })
-                  )
-                }, 
-              child: const Text(
-                  'Log in',
-                  style: TextStyle(
-                    color: Colors.teal,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
+                
+                SizedBox(height: 10),
+                
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    // padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    minimumSize: Size(double.infinity, 50)
+                  ),
+                  onPressed: () => {
+                    Navigator.push(context, 
+                      MaterialPageRoute(builder: (context) {
+                        return LoginPage();
+                      })
+                    )
+                  }, 
+                child: const Text(
+                    'Log in',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
